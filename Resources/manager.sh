cb0cff380ae56fce779d619f51089adc68ce3693
Amir Arbel <amirarbel@gmail.com>
2016-05-02 12:44:47 +0300

    Change how encryption works?

--- Resources/manager.sh
+++ Resources/manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget http://malachi-amir-bucket.s3.amazonaws.com/manager.enc
-openssl -d -out manager.jar -in manager.enc -pass $$filePassword$$
+openssl des -d -out manager.jar -in manager.enc -k $$filePassword$$
 java -jar manager.jar $$missionsPerWorker$$
--- Resources/worker.sh
+++ Resources/worker.sh
@@ -4,6 +4,6 @@ wget http://repo1.maven.org/maven2/com/googlecode/efficient-java-matrix-library/
 wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0.jar  
 wget http://central.maven.org/maven2/de/jollyday/jollyday/0.4.7/jollyday-0.4.7.jar  
 wget http://malachi-amir-bucket.s3.amazonaws.com/worker.enc
-openssl -d -out worker.jar -in worker.enc -pass $$filePassword$$
+openssl des -d -out worker.jar -in worker.enc -k $$filePassword$$
 java -Xms128m -Xmx768M -cp .:worker.jar:stanford-corenlp-3.3.0.jar:stanford-corenlp-3.3.0-models.jar:ejml-0.23.jar:jollyday-0.4.7.jar Analyzer
 touch done

6e95f1e1f14495d9e98e3b423a283d9b33a07749
Amir Arbel <amirarbel@gmail.com>
2016-05-03 20:29:34 +0300

    Figure out why nothing worked, lol

--- Resources/manager.sh
+++ Resources/manager.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/
 wget http://malachi-amir-bucket.s3.amazonaws.com/manager.enc
 openssl des -d -out manager.jar -in manager.enc -k $$filePassword$$
-java -jar manager.jar $$missionsPerWorker$$
+java -jar manager.jar $$missionsPerWorker$$ $$filePassword$$
